0f53da3ad695f4357168bfc97b83bccc0917480b
JT Wright <jacob.t.wright@ucdenver.edu>
2021-07-28 23:24:11 -0500

    really bad because we should be using this

--- app.js
+++ app.js
@@ -29,33 +29,29 @@ client.on('message', (msg) => {
     else if (msg.content === '!patch subscribe') {
         let subscribedChannel = msg.channel.id;
         client.channels.cache.get(subscribedChannel).send('Subscribed to #' + msg.channel.name + ' in ' + msg.channel.guild.name);
-        let lastPatchId = "";
-        setInterval(() => {
+        let lastPatchId = ""
+        const patchPromise = new Promise((resolve, reject) => {
+            setInterval(() => {
             if (subscribedChannel != "") {
-                lastPatchId = checkForPatch(lastPatchId, subscribedChannel);
+                fetch("http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=570&count=100&format=json")
+                .then(data => data.json())
+                .then(data => {
+                    const newsArr = data.appnews.newsitems;
+                    const newestNews = newsArr.find(n => {
+                        return n.feed_type === 1;
+                    })
+                    return newestNews;
+                })
+                .then(data => {
+                    if (data.gid !== lastPatchId) {
+                        client.channels.cache.get(subscribedChannel).send(data.url);
+                        lastPatchId = data.gid;
+                    }
+                })
             }
         }, 5000)
+    })
     }
 })
-
-function checkForPatch(lastPatchId, subscribedChannel) {
-    fetch("http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=570&count=100&format=json")
-        .then(data => data.json())
-        .then(data => {
-            const newsArr = data.appnews.newsitems;
-            const newestNews = newsArr.find(n => {
-                return n.feed_type === 1;
-            })
-            return newestNews;
-        })
-        .then(data => {
-            if (data.gid !== lastPatchId) {
-                client.channels.cache.get(subscribedChannel).send(data.url);
-                return data.gid;
-            }
-        })
-
-}
-
 
 client.login(process.env.BOT_TOKEN)
