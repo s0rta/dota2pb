0dc264cf8321ced2bfa9fcf1229a2f97d4504bf6
Marguerite Brugger <margueritebrugger@Marguerites-MacBook-Air.local>
2021-07-28 23:00:24 -0500

    bottttt is made mostly, but still sends patch note infinite times

--- app.js
+++ app.js
@@ -1,24 +1,61 @@
-const Discord = require('discord.js')
-const Parser = require('rss-parser')
+const Discord = require('discord.js');
+const fetch = require('node-fetch');
 
-let parser = new Parser()
+require('dotenv').config();
 
-require('dotenv').config()
-
-const client = new Discord.Client()
+const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('Bot is ready')
+    console.log('Bot is ready');
 })
 
 client.on('message', (msg) => {
     if (msg.content === '!patch') {
-        parser.parseURL("https://steamdb.info/api/PatchnotesRSS/?appid=570")
+        fetch("http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=570&count=100&format=json")
+            .then(data => data.json())
             .then(data => {
-                msg.reply(data.items[0].link)
+                const newsArr = data.appnews.newsitems;
+                const newestNews = newsArr.find(n => {
+                    return n.feed_type === 1;
+                })
+                console.log(newestNews);
+                return newestNews;
             })
+            .then(data => msg.reply(data.url))
+    }
+    else if (msg.content === '!patch help') {
+        msg.reply('Subscribed to #' + msg.channel.name + ' in ' + msg.channel.guild.name);
+    }
+    else if (msg.content === '!patch subscribe') {
+        let subscribedChannel = msg.channel.id;
+        client.channels.cache.get(subscribedChannel).send('Subscribed to #' + msg.channel.name + ' in ' + msg.channel.guild.name);
+        let lastPatchId = "";
+        setInterval(() => {
+            if (subscribedChannel != "") {
+                lastPatchId = checkForPatch(lastPatchId, subscribedChannel);
+            }
+        }, 5000)
     }
 })
+
+function checkForPatch(lastPatchId, subscribedChannel) {
+    fetch("http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=570&count=100&format=json")
+        .then(data => data.json())
+        .then(data => {
+            const newsArr = data.appnews.newsitems;
+            const newestNews = newsArr.find(n => {
+                return n.feed_type === 1;
+            })
+            return newestNews;
+        })
+        .then(data => {
+            if (data.gid !== lastPatchId) {
+                client.channels.cache.get(subscribedChannel).send(data.url);
+                return data.gid;
+            }
+        })
+
+}
 
 
 client.login(process.env.BOT_TOKEN)
